ddc86b6c045b536bd2cc6ef23e6e88420034e5b9
Steven Owen <stevenowen83@gmail.com>
2019-05-02 15:06:47 -0600

    added detail button to link to product detail

--- src/ProductList.js
+++ src/ProductList.js
@@ -4,35 +4,42 @@ import store from './store';
 
 class ProductList extends Component {
   handleClick(id) {
-    console.log(this.props);
     this.props.history.push('/products/' + id);
   }
 
   render() {
     return store.getState().products.map((product, idx) => (
       <div className="ui centered cards" key={idx}>
-        <div
-          className="ui raised link card"
-          onClick={() => this.handleClick(product.id)}
-        >
+        <div className="ui raised link card">
           <div className="ui centered small images">
             <img alt={product.title} src={product.img} />
           </div>
           <div className="content">
             <div className="center aligned header">{product.title}</div>
             <div className="meta">{product.category.toUpperCase()}</div>
+            <button
+              className="right floated ui blue mini button"
+              onClick={() => this.handleClick(product.id)}
+            >
+              Details...
+            </button>
           </div>
-          <div
-            className="extra content"
-            onClick={() => {
-              console.log('hit');
-            }}
-          >
-            <span className="right floated star">
+          <div className="extra content">
+            <span
+              className="right floated star"
+              onClick={() => {
+                console.log('favorite');
+              }}
+            >
               <i className="star icon" />
               Favorite
             </span>
-            <span className="floated star">
+            <span
+              className="floated star"
+              onClick={() => {
+                console.log('add to cart');
+              }}
+            >
               <i className="cart icon" />
               Add To Cart
             </span>
