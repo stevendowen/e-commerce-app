9579467ca18ffd63910c78c707577039aef3c032
Steven Owen <stevenowen83@gmail.com>
2019-05-02 08:38:08 -0600

    created router and added link and route to ProductList

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import ProductList from './ProductList';
 
 class App extends Component {
-  state = {
-    products: [],
-  };
-
-  componentDidMount() {
-    fetch('https://my-json-server.typicode.com/tdmichaelis/typicode/products')
-      .then(response => {
-        return response.json();
-      })
-      .then(myJson => {
-        this.setState({ products: myJson });
-      });
-  }
-
   render() {
     return (
-      <div>
-        <div className="ui inverted large menu">
-          <span className="item">Electronic Store</span>
-          <div className="right item">
-            <i className="large floated left cart icon" />
-            <div className="ui icon input">
-              <input type="text" placeholder="Search..." />
-              <i className="search icon" />
+      <Router>
+        <div>
+          <div className="ui inverted large menu">
+            <span className="item">Electronic Store</span>
+            <Link className="item" to="/">
+              Home
+            </Link>
+            <div className="right item">
+              <i className="large floated left cart icon" />
+              <div className="ui icon input">
+                <input type="text" placeholder="Search..." />
+                <i className="search icon" />
+              </div>
             </div>
           </div>
+          <Switch>
+            <Route exact path="/" component={ProductList} />
+          </Switch>
         </div>
-        <div className="ui centered cards">
-          <ProductList products={this.state.products} />
-        </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,28 +1,45 @@
 import React from 'react';
 
-const ProductList = props => {
-  return props.products.map((product, idx) => (
-    <div className="ui raised link card" key={idx}>
-      <div className="ui centered small images">
-        <img alt={product.title} src={product.img} />
+class ProductList extends React.Component {
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    fetch('https://my-json-server.typicode.com/tdmichaelis/typicode/products')
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({ products: myJson });
+      });
+  }
+
+  render() {
+    return this.state.products.map((product, idx) => (
+      <div className="ui centered cards">
+        <div className="ui raised link card" key={idx}>
+          <div className="ui centered small images">
+            <img alt={product.title} src={product.img} />
+          </div>
+          <div className="content">
+            <div className="center aligned header">{product.title}</div>
+            <div className="meta">{product.category.toUpperCase()}</div>
+          </div>
+          <div className="extra content">
+            <span className="right floated star">
+              <i className="star icon" />
+              Favorite
+            </span>
+            <span className="floated star">
+              <i className="cart icon" />
+              Add To Cart
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="content">
-        <div className="center aligned header">{product.title}</div>
-        <div className="meta">{product.category.toUpperCase()}</div>
-        <div className="center aligned description">{product.description}</div>
-      </div>
-      <div className="extra content">
-        <span className="right floated star">
-          <i className="star icon" />
-          Favorite
-        </span>
-        <span className="floated star">
-          <i className="cart icon" />
-          Add To Cart
-        </span>
-      </div>
-    </div>
-  ));
-};
+    ));
+  }
+}
 
 export default ProductList;
